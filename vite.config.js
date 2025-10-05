cb5d2830255a80cd7dd6e0773686fb89ec225da4
Terence Horsman <terence@horsman.com>
2025-10-05 16:32:47 +0100

    pages

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+export default defineConfig({
+    base: "/ChemoCare/", // <-- repo name
+    plugins: [react()],
+    resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } }
+});
